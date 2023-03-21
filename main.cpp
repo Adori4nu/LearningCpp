04f4681254e02252dd45c060ca1d7c03770801d3
Adori4nu <adriankowarik@gmail.com>
2023-03-21 19:56:38 +0100

    The cherno junky log class example

--- main.cpp
+++ main.cpp
@@ -54,17 +54,34 @@ void Move(Player& player, int xa, int ya)
 
 class Log
 {
+public:
+    const int LogLevelError = 0;
+    const int LogLevelWarning = 1;
+    const int LogLevelInfo = 2;
 private:
-    
+    int m_LogLevel = LogLevelInfo;
 public:
     void SetLevel(int level)
     {
+        m_LogLevel = level;
+    }
 
+    void Error(const char* message)
+    {
+        if (m_LogLevel >= LogLevelError)
+            std::cout << "[ERROR]: "<< message << std::endl;
     }
 
     void Warn(const char* message)
     {
+        if (m_LogLevel >= LogLevelWarning)
+            std::cout << "[WARNING]: "<< message << std::endl;
+    }
 
+    void Info(const char* message)
+    {
+        if (m_LogLevel >= LogLevelInfo)
+            std::cout << "[INFO]: "<< message << std::endl;
     }
 };
 
@@ -121,8 +138,10 @@ int main()
     }
 
     Log log;
-    log.SetLevel(LogLevelWarning);
+    log.SetLevel(log.LogLevelWarning);
+    log.Error("Hello Darknes!");
     log.Warn("Hello Darknes!");
+    log.Info("Hello Darknes!");
     std::cout << "Hello world!" << std::endl;
     std::cin.get();
     std::cout << MultiplyR(2,2);
