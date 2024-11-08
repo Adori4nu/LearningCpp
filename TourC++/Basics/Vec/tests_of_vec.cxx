c121278245dc3ddd04bad9359d383980fd4b5df7
Adori4nu <53657830+Adori4nu@users.noreply.github.com>
2024-11-08 18:35:16 +0100

    finaly got the modules working no need for any additional flags in msvc if module interface file has .ixx extension

--- TourC++/Basics/Vec/tests_of_vec.cxx
+++ TourC++/Basics/Vec/tests_of_vec.cxx
@@ -10,16 +10,19 @@ import Vector;
 
 int main()
 {
+    double sum_of_vec{};
+
 #if VEC_IMP == 22 || VEC_IMP == 23
 
-    double sum_of_vec{};
     read_and_sum(sum_of_vec, 3);
     std::cout << "Sum of input: " << sum_of_vec << std::endl;
 
 #endif
 #if VEC_IMP == 30
-    Vector<int> vec{3};
+
     std::cout << "Podrozdział 3" << std::endl;
+    read_and_sum(sum_of_vec, 3);
+    
 #endif
     std::cin.get();
 }
