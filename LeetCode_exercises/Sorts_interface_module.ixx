f5c2c42f901f004e6a3579cbe4c7c1a3127cc2ad
Adori4nu <53657830+Adori4nu@users.noreply.github.com>
2025-03-08 19:57:44 +0100

    selection and insertion sorts

--- LeetCode_exercises/Sorts_interface_module.ixx
+++ LeetCode_exercises/Sorts_interface_module.ixx
@@ -7,7 +7,7 @@ export module Sorts;
 
 export namespace my_junk
 {
-    #pragma region Buble Sort
+    #pragma region Bubble Sort
     template <typename T>
     auto bubble_sort(Vector<T>& container) -> void {
         for (size_t i{container.Size() - 1}; i > 0; --i) {
@@ -19,6 +19,45 @@ export namespace my_junk
                     // std::swap(container[j], container[j+1]); // or
                 }
             }
+        }
+    }
+    #pragma endregion
+    #pragma region Selection Sort
+    template <typename T>
+    auto selection_sort(Vector<T>& container) -> void {
+        for (size_t i{0}; i < container.Size() - 1; ++i) {
+            size_t min_index{i};
+            for (size_t j{i+1}; j < container.Size(); ++j) {
+                if (container[j] < container[min_index]) {
+                    min_index = j;
+                }
+            }
+            if (min_index != i) {
+                T temp{ std::move(container[i]) };
+                container[i] = std::move(container[min_index]);
+                container[min_index] = std::move(temp);
+            }
+        }
+    }
+    #pragma endregion
+    #pragma region Insertion Sort
+    template <typename T>
+    auto insertion_sort(Vector<T>& container) -> void {
+        for (size_t i{1}; i < container.Size(); ++i) {
+            // picking up the key
+            T key{ std::move(container[i]) };
+            size_t j{i};
+
+            // overriding element with the value of preivous one
+            // if its bigger than current key val
+            // j > 0 must be the begining condition
+            while (j > 0 && container[j - 1] > key) {
+                container[j] = std::move(container[j - 1]);
+                --j;
+            }
+
+            // storing the key at proper index for it
+            container[j] = std::move(key);
         }
     }
     #pragma endregion
---
+++ LeetCode_exercises/test_of_Sorts.cxx
@@ -0,0 +1,82 @@
+#include <cassert>
+#include <iostream>
+#include <print>
+#include "../Vector.hpp"
+
+import Test_helpers;
+import Sorts;
+
+auto main() -> int
+{
+    Vector<int> my_vec{};
+    my_vec.EmplaceBack(3);
+    my_vec.EmplaceBack(6);
+    my_vec.EmplaceBack(4);
+    my_vec.EmplaceBack(2);
+    my_vec.EmplaceBack(1);
+    my_vec.EmplaceBack(5);
+
+    std::println("\t\t\033[1;36m--- [Test of Sorts] ---\033[0m\n");
+    std::println("\033[1;33m--- [Bubble sort] ---\033[0m");
+    std::cout << "\033[34mBefore sorting:\033[0m ";
+    for (const auto& val : my_vec)
+    {
+        std::cout << val << " ";
+    }
+    my_junk::bubble_sort(my_vec);
+    std::cout << "\n\033[35mAfter sorting: \033[0m ";
+    for (size_t i{0}; i < my_vec.Size(); ++i)
+    {
+        if (my_vec[i] == int(i) + 1)
+            std::cout << "\033[32m"<< my_vec[i] << "\033[0m ";
+        else if (my_vec[i] != int(i) + 1)
+            std::cout << "\033[1;31m"<< my_vec[i] << "\033[0m ";
+    }
+    std::cout << "\n\n";
+
+    std::println("\033[1;33m--- [Selection sort] ---\033[0m");
+    my_vec[0] = 3;
+    my_vec[1] = 6;
+    my_vec[2] = 4;
+    my_vec[3] = 2;
+    my_vec[4] = 1;
+    my_vec[5] = 5;
+    std::cout << "\033[34mBefore sorting:\033[0m ";
+    for (const auto& val : my_vec)
+    {
+        std::cout << val << " ";
+    }
+    my_junk::selection_sort(my_vec);
+    std::cout << "\n\033[35mAfter sorting: \033[0m ";
+    for (size_t i{0}; i < my_vec.Size(); ++i)
+    {
+        if (my_vec[i] == int(i) + 1)
+            std::cout << "\033[32m"<< my_vec[i] << "\033[0m ";
+        else if (my_vec[i] != int(i) + 1)
+            std::cout << "\033[1;31m"<< my_vec[i] << "\033[0m ";
+    }
+    std::cout << "\n\n";
+
+    std::println("\033[1;33m--- [Insertion sort] ---\033[0m");
+    my_vec[0] = 3;
+    my_vec[1] = 6;
+    my_vec[2] = 4;
+    my_vec[3] = 2;
+    my_vec[4] = 1;
+    my_vec[5] = 5;
+    std::cout << "\033[34mBefore sorting:\033[0m ";
+    for (const auto& val : my_vec)
+    {
+        std::cout << val << " ";
+    }
+    my_junk::insertion_sort(my_vec);
+    std::cout << "\n\033[35mAfter sorting: \033[0m ";
+    for (size_t i{0}; i < my_vec.Size(); ++i)
+    {
+        if (my_vec[i] == int(i) + 1)
+            std::cout << "\033[32m"<< my_vec[i] << "\033[0m ";
+        else if (my_vec[i] != int(i) + 1)
+            std::cout << "\033[1;31m"<< my_vec[i] << "\033[0m ";
+    }
+    std::cout << "\n\n";
+}
